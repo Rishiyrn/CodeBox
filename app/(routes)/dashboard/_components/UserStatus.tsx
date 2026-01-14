53ab7097c0fb9946cef656d8612b91850411b645
Rishiyrn <rishiprasad524@gmail.com>
2026-01-14 23:05:59 +0530

    Update app/(routes)/dashboard/_components/UserStatus.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/(routes)/dashboard/_components/UserStatus.tsx
+++ app/(routes)/dashboard/_components/UserStatus.tsx
@@ -25,14 +25,14 @@ function UserStatus() {
             </div>
         </div>
         <div className="flex gap-3 items-center">
-            <Image src={'/badge.png'} alt="star" width={35} height={35}/>
+            <Image src={'/badge.png'} alt="badge" width={35} height={35}/>
             <div>
                 <h2 className="text-3xl font-game">3</h2>
                 <h2 className="font-game text-xl text-gray-500">Badge</h2>
             </div>
         </div>
         <div className="flex gap-3 items-center">
-            <Image src={'/fire.png'} alt="star" width={35} height={35}/>
+            <Image src={'/fire.png'} alt="fire" width={35} height={35}/>
             <div>
                 <h2 className="text-3xl font-game">7</h2>
                 <h2 className="font-game text-xl text-gray-500">Daily Streak</h2>
