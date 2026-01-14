7c6af83f0c67b68eabbcafe3cdee50818036192e
Rishi Prasad <rishiprasad524@gmail.com>
2026-01-15 00:45:18 +0530

    All courses display

---
+++ app/api/course/route.ts
@@ -0,0 +1,10 @@
+import { db } from "@/config/db";
+import { CourseTable } from "@/config/schema";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest){
+
+    const result = await db.select().from(CourseTable);
+
+    return NextResponse.json(result);
+}
