cd7cdba88a279f75b1f09e1c43b284428d762275
Rishi Prasad <rishiprasad524@gmail.com>
2026-01-14 22:53:29 +0530

    Dashboard Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Jersey_10 } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Jersey_10,} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/provider";
 import { ClerkProvider } from "@clerk/nextjs";
