0899778806bd57ee89738d1b2fb44a989a75e310
Rishiyrn <rishiprasad524@gmail.com>
2026-01-20 16:34:46 +0530

    Update app/(routes)/dashboard/_components/EnrolledCourses.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/(routes)/dashboard/_components/EnrolledCourses.tsx
+++ app/(routes)/dashboard/_components/EnrolledCourses.tsx
@@ -43,8 +43,9 @@ function EnrolledCourses() {
   return (
     <div className="mt-8">
       <h2 className="text-3xl mb-2 font-game">Your Enrolled Courses</h2>
-      {loading && <Skeleton className="w-full rounded-2xl my-5" />}
-      {enrolledCourses?.length == 0 ? (
+      {loading ? (
+        <Skeleton className="w-full rounded-2xl my-5" />
+      ) : enrolledCourses?.length == 0 ? (
         <div className="flex flex-col items-center gap-3 p-7 border rounded-2xl bg-zinc-900">
           <Image src={"/books.png"} alt="books" height={90} width={90} />
           <h2 className="font-game text-2xl">
